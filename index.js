4f349ddbec0b95dc3dd81b2fe1b1342b124f36ff
Arthur <arthurhcbp@gmail.com>
2021-01-22 19:31:39 -0300

    :camera:img gallery

---
+++ index.js
@@ -0,0 +1,24 @@
+  const panels = document.querySelectorAll('.panel');
+
+  
+  function toggleOpen() {
+      
+      this.classList.toggle('open');
+  }
+  
+  function toggleActive(e) {
+      
+      if (e.propertyName.includes('flex')) {
+          this.classList.toggle('open-active');
+      }
+
+  }
+  
+  
+  function init() {
+    panels.forEach(panel => panel.addEventListener('click', toggleOpen));
+    panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
+  }
+  
+  
+  init();
